78291f5616b8dd1e1b4e51b164c7c12a24a88d17
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2024-11-08 23:51:57 +0300

    Bilgisayar ve kullanici java class yapilari eklendi. Oyuncu sınıfına atamalar yapildi.

---
+++ src/Bilgisayar.java
@@ -0,0 +1,7 @@
+public class Bilgisayar extends Oyuncu {
+    public Bilgisayar(int oyuncuID, String oyuncuAdi, int skor){
+        super(oyuncuID, oyuncuAdi, skor) ;
+           
+    }
+
+}
---
+++ src/Kullanici.java
@@ -0,0 +1,7 @@
+
+public class Kullanici extends Oyuncu{
+
+    public Kullanici(int oyuncuID, String oyuncuAdi, int skor ){
+        super (oyuncuID,oyuncuAdi,skor);
+    }
+}
